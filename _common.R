2ff7a2adb9fc4f09b6b2d5e4e1db10e6ec9f8598
aakosm <aakos.mate@gmail.com>
2021-05-10 14:50:53 +0200

    digits to 3

--- _common.R
+++ _common.R
@@ -11,5 +11,7 @@ knitr::opts_chunk$set(
   fig.topcaption=TRUE
 )
 
+options(digits = 3, dplyr.print_min = 6, dplyr.print_max = 6)
+options(crayon.enabled = FALSE)
 suppressPackageStartupMessages(library(tidyverse))
 theme_set(theme_light())
